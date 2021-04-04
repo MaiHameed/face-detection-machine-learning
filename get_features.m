185a924f4ca39efa4c31a7424914f7dd0f39eeec
Mai Hameed <maihameed@hotmail.com>
2021-04-04 18:47:52 -0400

    Finished A3 Q1 roughly, might revisit

--- get_features.m
+++ get_features.m
@@ -1,6 +1,9 @@
-close all
-clear
-run('../vlfeat-0.9.20/toolbox/vl_setup')
+%% Run if VLFeat is not set up
+run('vlfeat-0.9.21/toolbox/vl_setup');
+
+%% Get Features
+close all;
+clear;
 
 pos_imageDir = 'cropped_training_images_faces';
 pos_imageList = dir(sprintf('%s/*.jpg',pos_imageDir));
@@ -10,15 +13,18 @@ neg_imageDir = 'cropped_training_images_notfaces';
 neg_imageList = dir(sprintf('%s/*.jpg',neg_imageDir));
 neg_nImages = length(neg_imageList);
 
-cellSize = 6;
-featSize = 31*cellSize^2;
+% cellSize needs to be a factor of 36
+cellSize = 4;
+im = imread(sprintf('%s/%s',pos_imageDir,pos_imageList(1).name));
+[imRows, imCols, ~] = size(im);
+featSize = 31*(imRows/cellSize)*(imCols/cellSize);
 
 pos_feats = zeros(pos_nImages,featSize);
 for i=1:pos_nImages
     im = im2single(imread(sprintf('%s/%s',pos_imageDir,pos_imageList(i).name)));
     feat = vl_hog(im,cellSize);
     pos_feats(i,:) = feat(:);
-    fprintf('got feat for pos image %d/%d\n',i,pos_nImages);
+%    fprintf('got feat for pos image %d/%d\n',i,pos_nImages);
 %     imhog = vl_hog('render', feat);
 %     subplot(1,2,1);
 %     imshow(im);
@@ -32,7 +38,7 @@ for i=1:neg_nImages
     im = im2single(imread(sprintf('%s/%s',neg_imageDir,neg_imageList(i).name)));
     feat = vl_hog(im,cellSize);
     neg_feats(i,:) = feat(:);
-    fprintf('got feat for neg image %d/%d\n',i,neg_nImages);
+%    fprintf('got feat for neg image %d/%d\n',i,neg_nImages);
 %     imhog = vl_hog('render', feat);
 %     subplot(1,2,1);
 %     imshow(im);
@@ -41,4 +47,4 @@ for i=1:neg_nImages
 %     pause;
 end
 
-save('pos_neg_feats.mat','pos_feats','neg_feats','pos_nImages','neg_nImages')
+save('pos_neg_feats.mat','pos_feats','neg_feats','pos_nImages','neg_nImages');
--- train_svm.m
+++ train_svm.m
@@ -1,13 +1,33 @@
-run('../vlfeat-0.9.20/toolbox/vl_setup')
-load('pos_neg_feats.mat')
+%% Run if VLFeat is not set up
+run('vlfeat-0.9.20/toolbox/vl_setup');
 
-feats = cat(1,pos_feats,neg_feats);
-labels = cat(1,ones(pos_nImages,1),-1*ones(neg_nImages,1));
+%% Split into training and validation
+close all;
+clear;
 
-lambda = 0.1;
-[w,b] = vl_svmtrain(feats',labels',lambda);
+load('pos_neg_feats.mat');
+
+percentTraining = 80;
+numOfTraining = floor((percentTraining/100)*pos_nImages);
+
+trainFeats = cat(1,pos_feats(1:numOfTraining,:), ...
+                    neg_feats(1:numOfTraining,:));
+validFeats = cat(1,pos_feats(1+numOfTraining:end,:), ...
+                    neg_feats(1+numOfTraining:end,:));
+trainLabels = cat(1,ones(numOfTraining,1), ...
+                    -1*ones(numOfTraining,1));
+validLabels = cat(1,ones(pos_nImages-numOfTraining,1), ...
+                    -1*ones(pos_nImages-numOfTraining,1));
+             
+%% Train
+
+lambda = 0.06;
+[w,b] = vl_svmtrain(trainFeats',trainLabels',lambda);
 
 fprintf('Classifier performance on train data:\n')
-confidences = [pos_feats; neg_feats]*w + b;
+confidences = [pos_feats(1:numOfTraining,:); neg_feats(1:numOfTraining,:)]*w + b;
+[tp_rate, fp_rate, tn_rate, fn_rate] =  report_accuracy(confidences, trainLabels);
 
-[tp_rate, fp_rate, tn_rate, fn_rate] =  report_accuracy(confidences, labels);
+fprintf('Classifier performance on validation data:\n')
+confidences = [pos_feats(1+numOfTraining:end,:); neg_feats(1+numOfTraining:end,:)]*w + b;
+[tp_rate, fp_rate, tn_rate, fn_rate] =  report_accuracy(confidences, validLabels);
